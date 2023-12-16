404415ab440d4fb76b943be18b900c02bef47e9a
1Basco <lsos13@gmail.com>
2023-12-16 15:41:37 -0300

    fix: add cors middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { useRoutes } from "./routes";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
